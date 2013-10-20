c011b9d27458d108a1d3aefd57738e359d0ff1e0
kevin <kevinnoll90@gmail.com>
2013-10-20 15:18:33 +0200

    oberfläche erstellt

--- Uebung1/src/main/java/mobile/app/dev/uebung1/MainActivity.java
+++ Uebung1/src/main/java/mobile/app/dev/uebung1/MainActivity.java
@@ -3,13 +3,58 @@ package mobile.app.dev.uebung1;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+    }
+
+    public void onClick(View v){
+        switch (v.getId()){
+            case R.id.button_0:
+                textView.append("0"); break;
+            case R.id.button_1:
+                textView.append("1"); break;
+            case R.id.button_2:
+                textView.append("2"); break;
+            case R.id.button_3:
+                textView.append("3"); break;
+            case R.id.button_4:
+                textView.append("4"); break;
+            case R.id.button_5:
+                textView.append("5"); break;
+            case R.id.button_6:
+                textView.append("6"); break;
+            case R.id.button_7:
+                textView.append("7"); break;
+            case R.id.button_8:
+                textView.append("8"); break;
+            case R.id.button_9:
+                textView.append("9"); break;
+            case R.id.button_add:
+                textView.append("+"); break;
+            case R.id.button_mult:
+                textView.append("*"); break;
+            case R.id.button_sub:
+                textView.append("-"); break;
+            case R.id.button_back:
+                if(textView.length()>0){
+                    textView.setText(textView.getText().subSequence(0, textView.getText().length()-1));
+                }
+                break;
+            case R.id.button_div:
+                textView.append("%"); break;
+            case R.id.button_dot:
+                textView.append("."); break;
+        }
     }
 
 
